fa15e59e0c189f93b8d533caef9f3efbbe73f038
kbender2020 <k.bender2020@gmail.com>
2018-04-05 20:52:27 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 
 
 #include "msp.h"
-
+//Clock frequencies for MSP 432
 #define FREQ_1_5_MHZ 1500000;
 #define FREQ_3_MHZ 3000000;
 #define FREQ_6_MHZ 6000000;
@@ -9,9 +9,9 @@
 #define FREQ_24_MHZ 24000000;
 #define FREQ_48_MHZ 48000000;
 
-void delay_ms(int ms, int freq);
-void delay_us(int us, int freq);
-void set_DCO(int freq);
+void delay_ms(int ms, int freq);       // Delay function for specified number of milliseconds
+void delay_us(int us, int freq);       // Delay function for specified number of microseconds
+void set_DCO(int freq);                // Setting the internal DCO clock frequency
 
 int main(void) {
 
@@ -21,10 +21,10 @@ int main(void) {
    P2->SEL0 &= ~0x02;      // set P2.5 as GPIO
    P2->DIR  |= 0x02;       // set P2.1 as output
 
-   while (1) {
-      P2->OUT |= (1<<1);    // P2.1 on
+   while (1) {                         // Run infinitely
+      P2->OUT |= (1<<1);               // P2.1 on ofr X milliseconds
       delay_ms(500, FREQ_3_MHZ);
-      P2->OUT &= (1<<1);   // P2.1 off
+      P2->OUT &= (1<<1);               // P2.1 off for X milliseconds
       delay_ms(500, FREQ_3_MHZ);
     }
 }
@@ -45,22 +45,22 @@ void set_DCO(int freq){
         DCORSEL = 3
         break;
     case FREQ_24_MHZ:
-        DCOSEL = 4
+        DCORSEL = 4
         break;
     case FREQ_48_MHZ:
-        DCOSEL = 5
+        DCORSEL = 5
         break;
     }
 }*/
 
 // Delay milliseconds function
 void delay_ms(int ms, int freq) {
-    int num_cycles = (freq * ms)/1000;
-    __delay_cycles(num_cycles);
+    int num_cycles = (freq * ms)/1000;    // Calculate number of cycles needed to generate specified delay
+    __delay_cycles(num_cycles);           // Delay the device for that number of cycles
 }
 
 // Delay microseconds function
 void delay_us(int us, int freq){
-    int num_cycles = (freq * us)/1000000;
-    __delay_cycles(num_cycles);
+    int num_cycles = (freq * us)/1000000; // Calculate number of cycles needed to generate specified delay 
+    __delay_cycles(num_cycles);           // Delay the device for that number of cycles
 }
